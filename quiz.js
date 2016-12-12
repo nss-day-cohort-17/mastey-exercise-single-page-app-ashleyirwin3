084f32d43a27aa333d4e573f12f9090bc8d77c02
Ashley Irwin <ashleykayirwin3@gmail.com>
2016-12-12 15:32:00 -0600

    send an XMR request to load json content into console

--- quiz.js
+++ quiz.js
@@ -1,1 +1,12 @@
-console.log("heyo")
+console.log("heyo");
+
+function loadJSON(e) {
+        var data = JSON.parse(e.target.responseText)
+        console.log("data", data)
+}
+
+var myRequest = new XMLHttpRequest()
+
+myRequest.addEventListener("load", loadJSON)
+myRequest.open("GET", "inventory.json")
+myRequest.send()
