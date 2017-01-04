456370201008f15528e9c48456f8b8ce6dfcdd37
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-04 15:47:24 -0600

    fixed json parse

--- quiz.js
+++ quiz.js
@@ -1,12 +1,32 @@
 console.log("heyo");
 
-function loadJSON(e) {
-        var data = JSON.parse(e.target.responseText)
-        console.log("data", data)
-}
+var cars;
 
 var myRequest = new XMLHttpRequest()
 
-myRequest.addEventListener("load", loadJSON)
+function loadCars(e) {
+    var data = JSON.parse(e.target.responseText)
+    cars = data.cars
+    console.log("cars", cars)
+}
+
+  //   var data = ""
+  //   var loadCars = cars
+  //   for (var i = 0; i < data.cars; i++) {
+  //           loadCars += `<div>
+  //                           <make>${data.cars[i].make}</date>
+  //                           <model>${data.cars[i].model}</model>
+  //                           <year>${data.cars[i].year}</year>
+  //                           <price>${data.cars[i].price}</price>
+  //                           <description>${data.cars[i].description}</description>
+  //                       </div>`
+
+  // }
+    document.getElementById("cars_load").innerHTML = loadCars
+
+
+
+
+myRequest.addEventListener("load", loadCars)
 myRequest.open("GET", "inventory.json")
 myRequest.send()
